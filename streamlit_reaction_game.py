82612eeef0b7aeb055a93f05268b13c957803a49
useruser0012 <minjung20090616@gmail.com>
2025-05-23 10:57:58 +0900

    Update streamlit_reaction_game.py

--- streamlit_reaction_game.py
+++ streamlit_reaction_game.py
@@ -1,6 +1,4 @@
 import os
-import streamlit as st
 
 st.write("현재 작업 디렉토리:", os.getcwd())
-st.write("디렉토리 내 파일 목록:", os.listdir())
-st.write("json 파일 존재 여부:", os.path.isfile('statistics-festival-178f7f9532ad.json'))
+st.write("현재 디렉토리 파일 목록:", os.listdir())
