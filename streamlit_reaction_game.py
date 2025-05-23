29d6687113d91661c8d2885006edcaa5c5d95588
useruser0012 <minjung20090616@gmail.com>
2025-05-23 11:09:29 +0900

    Update streamlit_reaction_game.py

--- streamlit_reaction_game.py
+++ streamlit_reaction_game.py
@@ -1,10 +1,5 @@
-import gspread
-from google.oauth2.service_account import Credentials
+import json
 
-SERVICE_ACCOUNT_FILE = "/mount/src/statistics_festival/service_account.json"
-credentials = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=[
-    'https://www.googleapis.com/auth/spreadsheets',
-    'https://www.googleapis.com/auth/drive'
-])
-
-gc = gspread.authorize(credentials)  # 여기서 인증 시도됨
+with open("service_account.json", "r", encoding="utf-8") as f:
+    data = json.load(f)
+    print(data["private_key"])
