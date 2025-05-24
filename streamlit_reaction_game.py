5a449adaa2aa46993a243ca64f4ab8b5b11b86bf
useruser0012 <minjung20090616@gmail.com>
2025-05-25 00:44:17 +0900

    Update streamlit_reaction_game.py

--- streamlit_reaction_game.py
+++ streamlit_reaction_game.py
@@ -1,9 +1,11 @@
 from google.oauth2.service_account import Credentials
 import gspread
+import streamlit as st
 
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 
-creds = Credentials.from_service_account_file('statistics-festival-6037ec1a564b.json', scopes=scope)
+creds = Credentials.from_service_account_info(st.secrets["gcp_service_account"], scopes=scope)
 client = gspread.authorize(creds)
 
 sheet = client.open("도파민 타이밍 게임 기록").sheet1
+
